7cc52f43f1a31b5c1caca9c84e24540122579214
TPOCTO4KA <57991172+TPOCTO4KA@users.noreply.github.com>
2020-02-12 14:50:30 +0300

    Update and rename Задание 2.py to Задание 2 Урок 2.py

--- Задание 2 Урок 2.py
+++ Задание 2 Урок 2.py
@@ -5,3 +5,12 @@
 Для заполнения списка элементов необходимо использовать функцию input()
 '''
 
+user_answer = input 'Введите список через запятую'
+user_list = user_answer.split(',')  # Разделение введенного списка по заяпятой
+print user_list
+
+idx = 0  # Вводим индекс
+while idx < len(user_list[:-1]):
+  user_list[idx], user_list[idx+1] = user_list[idx+1], user_list[idx]
+  idx += 2
+print user_list
